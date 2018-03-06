b91bb39b3c885bcfb29c9a5c1c6e4215288b0d0c
cgebe <gebendorfer@icewave.org>
2018-03-06 16:35:50 +0100

    fu commas

--- tensor2tensor/bin/train_all.py
+++ tensor2tensor/bin/train_all.py
@@ -1,28 +1,29 @@
 import os
 
 TRANSLATE_PROBLEMS = [
-    "translate_csde_legal8k"
-    "translate_csen_legal8k"
-    "translate_cses_legal8k"
-    "translate_csfr_legal8k"
-    "translate_csit_legal8k"
-    "translate_cssv_legal8k"
-    "translate_deen_legal8k"
-    "translate_dees_legal8k"
-    "translate_defr_legal8k"
-    "translate_deit_legal8k"
-    "translate_desv_legal8k"
-    "translate_enes_legal8k"
-    "translate_enfr_legal8k"
-    "translate_enit_legal8k"
-    "translate_ensv_legal8k"
-    "translate_esfr_legal8k"
-    "translate_esit_legal8k"
-    "translate_essv_legal8k"
-    "translate_frit_legal8k"
-    "translate_frsv_legal8k"
+    "translate_csde_legal8k",
+    "translate_csen_legal8k",
+    "translate_cses_legal8k",
+    "translate_csfr_legal8k",
+    "translate_csit_legal8k",
+    "translate_cssv_legal8k",
+    "translate_deen_legal8k",
+    "translate_dees_legal8k",
+    "translate_defr_legal8k",
+    "translate_deit_legal8k",
+    "translate_desv_legal8k",
+    "translate_enes_legal8k",
+    "translate_enfr_legal8k",
+    "translate_enit_legal8k",
+    "translate_ensv_legal8k",
+    "translate_esfr_legal8k",
+    "translate_esit_legal8k",
+    "translate_essv_legal8k",
+    "translate_frit_legal8k",
+    "translate_frsv_legal8k",
     "translate_itsv_legal8k"
 ]
+
 
 
 def main():
