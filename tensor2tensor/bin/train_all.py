80535f6d0288cf90ee6ca9afe94b3a895ab29eb4
cgebe <gebendorfer@icewave.org>
2018-03-07 12:53:44 +0100

    32k subword vocab

--- tensor2tensor/bin/train_all.py
+++ tensor2tensor/bin/train_all.py
@@ -1,34 +1,33 @@
 import os
 
 TRANSLATE_PROBLEMS = [
-    "translate_csde_legal8k",
-    "translate_csen_legal8k",
-    "translate_cses_legal8k",
-    "translate_csfr_legal8k",
-    "translate_csit_legal8k",
-    "translate_cssv_legal8k",
-    "translate_deen_legal8k",
-    "translate_dees_legal8k",
-    "translate_defr_legal8k",
-    "translate_deit_legal8k",
-    "translate_desv_legal8k",
-    "translate_enes_legal8k",
-    "translate_enfr_legal8k",
-    "translate_enit_legal8k",
-    "translate_ensv_legal8k",
-    "translate_esfr_legal8k",
-    "translate_esit_legal8k",
-    "translate_essv_legal8k",
-    "translate_frit_legal8k",
-    "translate_frsv_legal8k",
-    "translate_itsv_legal8k"
+    "translate_csde_legal32k",
+    "translate_csen_legal32k",
+    "translate_cses_legal32k",
+    "translate_csfr_legal32k",
+    "translate_csit_legal32k",
+    "translate_cssv_legal32k",
+    "translate_deen_legal32k",
+    "translate_dees_legal32k",
+    "translate_defr_legal32k",
+    "translate_deit_legal32k",
+    "translate_desv_legal32k",
+    "translate_enes_legal32k",
+    "translate_enfr_legal32k",
+    "translate_enit_legal32k",
+    "translate_ensv_legal32k",
+    "translate_esfr_legal32k",
+    "translate_esit_legal32k",
+    "translate_essv_legal32k",
+    "translate_frit_legal32k",
+    "translate_frsv_legal32k",
+    "translate_itsv_legal32k"
 ]
-
 
 
 def main():
     for problem in TRANSLATE_PROBLEMS:
-        if os.system("python ./t2t-trainer --data_dir=$DATA_DIR --output_dir=$TRAIN_DIR --model=multi_model --worker_gpu 4 --hparams_set=multimodel_base --problems="+problem) == 0:
+        if os.system("python ./t2t-trainer --data_dir=$DATA_DIR/translate --output_dir=$TRAIN_DIR/translate/"+problem+" --model=multi_model --worker_gpu 4 --hparams_set=multimodel_base --problems="+problem) == 0:
             continue
         else:
             print "ERROR " + problem
