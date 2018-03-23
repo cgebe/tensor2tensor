e38b9ebfa68e57f39626eadf3ceffb4a1d64a027
cgebe <gebendorfer@icewave.org>
2018-03-23 13:28:50 +0100

    train all remove it

--- tensor2tensor/bin/train_all.py
+++ tensor2tensor/bin/train_all.py
@@ -6,22 +6,22 @@ TRANSLATE_PROBLEMS = [
     #"translate_cses_legal32k",
     #"translate_csfr_legal32k",
     #"translate_csit_legal32k",
-    "translate_cssv_legal32k",
-    "translate_deen_legal32k",
-    "translate_dees_legal32k",
-    "translate_defr_legal32k",
-    "translate_deit_legal32k",
+    #"translate_cssv_legal32k",
+    #"translate_deen_legal32k",
+    #"translate_dees_legal32k",
+    #"translate_defr_legal32k",
+    #"translate_deit_legal32k",
     "translate_desv_legal32k",
     "translate_enes_legal32k",
     "translate_enfr_legal32k",
-    "translate_enit_legal32k",
+    #"translate_enit_legal32k",
     "translate_ensv_legal32k",
     "translate_esfr_legal32k",
-    "translate_esit_legal32k",
+    #"translate_esit_legal32k",
     "translate_essv_legal32k",
-    "translate_frit_legal32k",
+    #"translate_frit_legal32k",
     "translate_frsv_legal32k",
-    "translate_itsv_legal32k"
+    #"translate_itsv_legal32k"
 ]
 
 
