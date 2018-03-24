1209c702a2048e9e8b543c894e2df35ced01828a
cgebe <gebendorfer@icewave.org>
2018-03-24 10:48:13 +0100

    fixes to training fails

--- tensor2tensor/bin/train_all.py
+++ tensor2tensor/bin/train_all.py
@@ -10,28 +10,38 @@ TRANSLATE_PROBLEMS = [
     #"translate_deen_legal32k",
     #"translate_dees_legal32k",
     #"translate_defr_legal32k",
-    #"translate_deit_legal32k",
+    "translate_deit_legal32k",
     "translate_desv_legal32k",
     "translate_enes_legal32k",
     "translate_enfr_legal32k",
-    #"translate_enit_legal32k",
+    "translate_enit_legal32k",
     "translate_ensv_legal32k",
     "translate_esfr_legal32k",
-    #"translate_esit_legal32k",
+    "translate_esit_legal32k",
     "translate_essv_legal32k",
-    #"translate_frit_legal32k",
+    "translate_frit_legal32k",
     "translate_frsv_legal32k",
-    #"translate_itsv_legal32k"
+    "translate_itsv_legal32k"
 ]
 
 
 def main():
     for problem in TRANSLATE_PROBLEMS:
         os.system("mkdir -p $TRAIN_DIR/translate/"+problem);
-        if os.system("python ./t2t-trainer --data_dir=$DATA_DIR/translate/"+problem+" --output_dir=$TRAIN_DIR/translate/"+problem+" --worker_gpu=4 --training_steps=250000 --model=multi_model --hparams_set=multimodel_legal --problems="+problem) == 0:
-            continue
-        else:
-            print "ERROR " + problem
-            break
+        successful = false
+        while not successful:
+            numbers = []
+            for f in os.listdir("$TRAIN_DIR/translate/"+problem):
+                if f.endswith(".index"):
+                    numbers.append(int(f.split("-")[1].split(".")[0]))
+            if (len(numbers)) > 0 :
+                steps = 250000 - max(numbers)
+            else:
+                steps = 250000
+
+            print(steps)
+            cmd = "python ./t2t-trainer --data_dir=$DATA_DIR/translate/"+problem+" --output_dir=$TRAIN_DIR/translate/"+problem+" --worker_gpu=4 --training_steps="+steps+" --model=multi_model --hparams_set=multimodel_legal --problems="+problem
+            if os.system(cmd) == 0:
+                successful = true
 
 main()
