234f876aa263304e4fe926453e251e1013e8a64a
cgebe <gebendorfer@icewave.org>
2018-05-26 10:51:21 +0200

    dgx joint all

---
+++ tensor2tensor/bin/dgx_translate_joint_all.py
@@ -0,0 +1,20 @@
+import os
+
+PROBLEMS = [
+    "translate_deen_legal32k",
+    "summarize_de_legal32k",
+    "multi_labeling_de_legal32k",
+]
+
+def main():
+    joint = ""
+    for problem in PROBLEMS:
+        joint += problem+"-"
+
+    joint = joint[:-1]
+    print(joint)
+    cmd = "python ./t2t-trainer --data_dir=$DATA_DIR --output_dir=$TRAIN_DIR/multimodel/all/de-only --worker_gpu=8 --train_steps=250000 --model=multi_model --hparams_set=multimodel_base --save_checkpoints_secs=1200 --schedule=train --problems="+joint
+    os.system(cmd)
+
+
+main()
